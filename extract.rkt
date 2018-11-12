0ccae723d7bab2660c57d576c15748fed88a5a75
Diego Zamboni <diego@zzamboni.org>
2018-11-12 22:38:40 +0100

    Ongoing learning Racket

---
+++ extract.rkt
@@ -0,0 +1,6 @@
+#!/usr/bin/env racket
+#lang racket
+(define (extract str)
+    (substring str 4 7))
+
+(extract "the cat out of the bag")
---
+++ hello.txt.pp
@@ -0,0 +1,4 @@
+#lang pollen
+Grand Illusion
+Pieces of Eight
+Paradise Theatre
