fc5f36e9311d6f09c18013d1bb051797498746d2
Diego Zamboni <diego@zzamboni.org>
2018-11-14 08:12:40 +0100

    More examples from "Beautiful Racket"

--- funstacker.rkt
+++ funstacker.rkt
@@ -2,35 +2,28 @@
 
 (define (read-syntax path port)
   (define src-lines (port->lines port))
-  (define src-datums (format-datums '(handle ~a) src-lines))
-  (define module-datum `(module stacker-mod "funstacker.rkt"
-                          ,@src-datums))
+  (define src-datums (format-datums '~a src-lines))
+  (define module-datum `(module funstacker-mod "funstacker.rkt"
+                          (handle-args ,@src-datums)))
   (datum->syntax #f module-datum))
 
 (provide read-syntax)
 
-(define-macro (stacker-module-begin HANDLE-EXPR ...)
+(define-macro (funstacker-module-begin HANDLE-ARGS-EXPR)
   #'(#%module-begin
-     HANDLE-EXPR ...
-     (display (first stack))))
-(provide (rename-out [stacker-module-begin #%module-begin]))
+     (display (first HANDLE-ARGS-EXPR))))
+(provide (rename-out [funstacker-module-begin #%module-begin]))
 
-(define stack empty)
-
-(define (pop-stack!)
-  (define arg (first stack))
-  (set! stack (rest stack))
-  arg)
-
-(define (push-stack! arg)
-  (set! stack (cons arg stack)))
-
-(define (handle [arg #f])
-  (cond
-    [(number? arg) (push-stack! arg)]
-    [(or (equal? + arg) (equal? * arg))
-     (define op-result (arg (pop-stack!) (pop-stack!)))
-     (push-stack! op-result)]))
-(provide handle)
+(define (handle-args . args)
+  (for/fold ([stack-acc empty])
+            ([arg (in-list args)]
+             #:unless (void? arg))
+    (cond
+      [(number? arg) (cons arg stack-acc)]
+      [(or (equal? * arg) (equal? + arg))
+       (define op-result
+         (arg (first stack-acc) (second stack-acc)))
+       (cons op-result (drop stack-acc 2))])))
+(provide handle-args)
 
 (provide + *)
